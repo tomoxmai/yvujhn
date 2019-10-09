933b658035bf3248fa3e390326498f301ece0f03
Alumno <Alumno@PC15>
2019-10-09 12:24:45 -0500

    mk

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void TxtEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
